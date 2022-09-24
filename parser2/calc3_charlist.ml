37f5cbe230f4bdde1cc47b27db6b200afeef35df
liunix <liuyifan.com@gmail.com>
2022-09-24 11:04:13 +0800

    tidy code

--- parser2/calc3_charlist.ml
+++ parser2/calc3_charlist.ml
@@ -1,17 +1,27 @@
-(* 来源: http://www.varesano.net/blog/fabio/simple%20math%20expression%20calculator%20ocaml 文章中有5种实现(已无法打开), 
+(* 来源: http://www.varesano.net/blog/fabio/simple%20math%20expression%20calculator%20ocaml 文章中有5种实现(已无法打开) *)
+
+(*
 这是第一种char list实现
 解析函数每个都解处理 [1; 0;  ; +;  ; 2; *; 3] 这样的list, 不符合常规用法
-
 ocamlc -g -o calc3_charlist.nc calc3_charlist.ml  # ctr+shift+b 编译
 F5 debug
 *)
+
+(* 表达式文法
+  expr = plus_expr | plus_expr + expr
+  plus_expr = mul_expr | mul_expr * plus_expr
+  mul_expr = number | ( expr )
+*)
+
+(* 字符串转char list: .[x]为下标访问*)
 let explode s =
   let rec loop acc = function
     | 0 -> s.[0] :: acc
     | x -> loop (s.[x] :: acc) (x - 1)
   in
-  loop [] (String.length s - 1)
+  loop [] ( (String.length s) - 1)
 
+(* char list转回string, String.make 1 char制作单字符串, ^为字符串拼接函数 *)
 let rec implode = function
   | [] -> ""
   | x :: xs -> String.make 1 x ^ implode xs
@@ -19,13 +29,8 @@ let rec implode = function
 let is_digit x = x >= '0' && x <= '9'
 let int_of_digit x = int_of_char x - int_of_char '0'
 
-(*
-  expr = plus_expr | plus_expr + expr
-  plus_expr = mul_expr | mul_expr * plus_expr
-  mul_expr = number | ( expr )
-*)
-
-exception Error of char list
+(* 自定义异常 *)
+exception SyntaxError of char list
 
 (* 基本元素 *)
 let number l =
@@ -43,8 +48,8 @@ let rec mul_expr l =
     let x, l = expr xs in
     ( match l with
     | ')' :: xs -> x, xs
-    | _ -> raise (Error l) )
-  | _ -> raise (Error l)
+    | _ -> raise (SyntaxError l) )
+  | _ -> raise (SyntaxError l)
 
 (* 加法处理 *)
 and plus_expr l =
@@ -71,8 +76,8 @@ let calc s =
     let x, l = expr l in
     match l with
     | [] -> Printf.printf "%d\n" x
-    | _ -> raise (Error l)
-  with Error l -> Printf.printf "Parse error at '%s'\n" (implode l)
+    | _ -> raise (SyntaxError l)
+  with SyntaxError l -> Printf.printf "Parse error at '%s'\n" (implode l)
 
 let _ =
   List.iter calc [ "1"; "1+1"; "2*3"; "(1)"; "1+2*3"; "2*(3+4)*5"; "2+3*4(5)" ]
